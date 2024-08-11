81a4d51535bc63a365c5d2b937d86b2e61afb844
SterrenHeer <sterrenheer@gmail.com>
2024-08-11 20:21:51 +0300

    add media for header section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 $.get('header.html',function(response){ 
     $('.header').html(response);
+    $('.burger_button, .burger_close, .burger a, .burger button').click(() => {
+        $('.burger').toggleClass('flex');
+    });
 });
